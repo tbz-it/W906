783c4fe2007d469583f65fc49e34fe406b335a86
Marcel <marcel.bernet@ch-open.ch>
2020-05-11 09:40:32 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,10 @@
 # 
 
 # K8s WebUI mit eigener README.md neu erzeugen
-sudo bash -x /opt/lernmaas/services/k8swebui.sh README.md minimal
+sudo bash -x /opt/lernmaas/services/k8swebui.sh README.md master
 
-# DevOps Umgebung
-kubectl apply -f ~/M426/
+# DevOps Umgebung nur fuer Host W906-02 setzen
+if [ $(hostname | cut -d- -f 2) -lt 3 ]
+then
+    kubectl apply -f .
+fi
